448cbc4da5cd886508862df7fdc042b85a5f98e4
Filippi <filippiusk@gmail.com>
2020-04-15 08:15:18 -0300

    WEB ADD: relacionamento de users com courses e incluido funcao de imprimir na lista de courses

--- web/js/controllers/courseFormController.js
+++ web/js/controllers/courseFormController.js
@@ -1,11 +1,21 @@
-angular.module('app').controller('courseFormCtrl', function ($scope, $stateParams, courseService) {
+angular.module('app').controller('courseFormCtrl', function ($scope, $stateParams, $element, courseService, userService) {
   $scope.course = {}
+  $scope.users = []
   $scope.loading = false
+  $scope.searchTerm
 
   $scope.fetch = function (id) {
     $scope.loading = courseService.fetchCourse(id).then(data => {
       $scope.loading = false
+      data.users = data.users.map(user => user.id)
       $scope.course = data
+    })
+  }
+
+  $scope.fetchUsers = function () {
+    $scope.loading = userService.fetchUsers().then(data => {
+      $scope.loading = false
+      $scope.users = data.data
     })
   }
 
@@ -16,8 +26,18 @@ angular.module('app').controller('courseFormCtrl', function ($scope, $stateParam
     })
   }
 
+  $scope.clearSearchTerm = function() {
+    $scope.searchTerm = '';
+  };
+
+  angular.element($element[0].querySelector('input.select-header-searchBox')).on('keydown', function(ev) {
+      ev.stopPropagation();
+  });
+
   const id = $stateParams.id
   if (id) {
     $scope.fetch(id)
   }
+
+  $scope.fetchUsers()
 })
--- web/js/controllers/courseListController.js
+++ web/js/controllers/courseListController.js
@@ -1,4 +1,4 @@
-angular.module('app').controller('courseListCtrl', function ($scope, $mdDialog, courseService) {
+angular.module('app').controller('courseListCtrl', function ($scope, $filter, $mdDialog, courseService) {
   $scope.loading = false
   $scope.courses = []
   $scope.query = {
@@ -33,6 +33,99 @@ angular.module('app').controller('courseListCtrl', function ($scope, $mdDialog, 
       }, function() {
         console.log('remove -> confirm -> false')
       })
+  }
+
+  $scope.showModalUsers = function (course) {
+    function DialogController($scope, $mdDialog, course) {
+      $scope.course = course;
+     
+      $scope.cancel = function() {
+        $mdDialog.hide();
+      }
+    }
+
+    $mdDialog.show({
+      templateUrl: 'view/courseModalUsers.html',
+      controller: DialogController,
+      clickOutsideToClose: true,
+      locals: {
+        course
+      }
+    })
+  }
+
+  $scope.print = async function () {
+    const courses = (await courseService.fetchCourses({limit: 99999, sort: 'id'})).data
+
+    let doc = new jsPDF('p', 'pt')
+    doc.setFontSize(12)
+    doc.setTextColor(0)
+    doc.setFontStyle('bold')
+    doc.text('Cursos', 40, 50)
+  
+    const courseHeader = ['ID', 'Título', 'Carga horária', 'Valor']
+    const courseKeys = ['id', 'title', 'workload', 'value']
+    const courseBody = getCourseBody()
+    doc.autoTable({
+      head: [courseHeader],
+      body: courseBody,
+      startY: 60
+    })
+
+    doc.text('Matriculas', 40, doc.autoTable.previous.finalY + 40)
+    
+    const userHeader = ['ID Curso', 'Nome', 'Email', 'Telefone']
+    const userKeys = ['name', 'email', 'phone']
+    const userBody = getUserBody()
+    doc.autoTable({
+      startY: doc.autoTable.previous.finalY + 50,
+      head: [userHeader],
+      body: userBody,
+      theme: 'grid'
+    })
+    
+    window.open(doc.output('bloburl'), '_blank')
+
+    function getCourseBody () {
+      return courses.map(item => {
+        return courseKeys.map(key => {
+          let data = item[key]
+          if (key === 'workload') data = $filter('durationTimeByMinutes')(data)
+          else if (key === 'value') data = $filter('currency')(data)
+  
+          return data
+        })
+      })
+    }
+
+    function getUserBody () {
+      let body = []
+      courses.forEach(course => {
+        if (course.users.length < 1) return
+
+        course.users.forEach((user, index) => {
+          var row = []
+
+          if (index < 1) {
+            row.push({
+              rowSpan: course.users.length,
+              content: course.id,
+              styles: { valign: 'middle', halign: 'center' }
+            })
+          }
+
+          userKeys.forEach(key => {
+            let data = user[key]
+            if (key === 'phone') data = $filter('phone')(data)
+            
+            row.push(data)
+          })
+          body.push(row)
+        })
+      })
+      
+      return body
+    }
   }
 
   $scope.fetch()
